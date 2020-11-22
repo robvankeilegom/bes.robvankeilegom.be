d9c04585dfc25fa83f14a9a44d77f240517b8763
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-11-22 21:52:24 +0100

    More formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit } from '@angular/core'
-import { ISetting, IVersnelling } from './shared/setting.model'
+import { Component, OnInit } from '@angular/core';
+import { ISetting, IVersnelling } from './shared/setting.model';
 import { IPlot, IData } from './shared/plot.model';
 
-import Chart from 'chart.js'
-import * as $ from 'jquery'
+import Chart from 'chart.js';
+import * as $ from 'jquery';
 import { IList, IListItem } from './shared/list.model';
 
-declare let toastr
+declare let toastr;
 
-@Component({
+@Component( {
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
+  styleUrls: [ './app.component.scss' ]
+} )
 export class AppComponent implements OnInit {
 
-  currentSetting: ISetting
-  plots: IPlot[] = []
-  lists: IList[] = []
-  maxPlots: number = 6
-  chart: any
+  currentSetting: ISetting;
+  plots: IPlot[] = [];
+  lists: IList[] = [];
+  maxPlots = 6;
+  chart: any;
 
   colors = [
     '#5481E6',
@@ -30,112 +30,116 @@ export class AppComponent implements OnInit {
     '#913CCD',
   ];
 
-  ngOnInit() {
-    this.currentSetting = this.defaultValue()
-    this.updateGraph()
+  ngOnInit () {
+    this.currentSetting = this.defaultValue();
+    this.updateGraph();
   }
 
-  updateCurrentSetting(setting: ISetting) {
-    this.currentSetting = setting
-    this.updateGraph()
+  updateCurrentSetting ( setting: ISetting ) {
+    this.currentSetting = setting;
+    this.updateGraph();
   }
 
-  saveAsNew(setting: ISetting) {
-    if (this.plots.length < this.maxPlots) {
-      toastr.success(setting.naam + ' opgeslagen.')
-      this.currentSetting = this.defaultValue()
-      this.updateGraph(true)
+  saveAsNew ( setting: ISetting ) {
+    if ( this.plots.length < this.maxPlots ) {
+      toastr.success( setting.naam + ' opgeslagen.' );
+      this.currentSetting = this.defaultValue();
+      this.updateGraph( true );
     } else {
-      toastr.error('Maximum ' + this.maxPlots + '!');
+      toastr.error( 'Maximum ' + this.maxPlots + '!' );
     }
   }
 
-  updateGraph(addNew?) {
-    addNew = addNew || false
+  updateGraph ( addNew?) {
+    addNew = addNew || false;
 
-    if (this.chart && typeof this.chart.destroy === 'function') {
+    if ( this.chart && typeof this.chart.destroy === 'function' ) {
       this.chart.destroy();
-      
+
     }
 
-    if (this.plots.length !== 0 && addNew === false) {
-      this.plots.pop()
+    if ( this.plots.length !== 0 && addNew === false ) {
+      this.plots.pop();
     }
 
-    if (this.lists.length !== 0 && addNew === false) {
-      this.lists.pop()
+    if ( this.lists.length !== 0 && addNew === false ) {
+      this.lists.pop();
     }
 
 
-    let dataset: IPlot = {
+    const dataset: IPlot = {
       label: this.currentSetting.naam,
       backgroundColor: 'rgba(0, 0, 0, 0)',
       borderWidth: 4,
-      borderColor: this.colors[this.plots.length % this.maxPlots],
+      borderColor: this.colors[ this.plots.length % this.maxPlots ],
       showLine: true,
       lineTension: 0,
       tension: 0,
       data: [],
-    }
+    };
 
-    dataset.data.push({
+    dataset.data.push( {
       x: 0,
       y: 0
-    })
+    } );
 
-    let dataElement: IData
-    let lastSpeed: number
-    let x: number
-    let vi: number = 1
-    let list: IList = {
+    let dataElement: IData;
+    let lastSpeed: number;
+    let x: number;
+    let vi = 1;
+    const list: IList = {
       naam: this.currentSetting.naam,
       color: dataset.borderColor,
       items: []
-    }
-    let listItem: IListItem
+    };
+    let listItem: IListItem;
 
-    this.currentSetting.versnellingen.forEach(versnelling => {
-      if (versnelling.waarde === 0) {
-        return
+    this.currentSetting.versnellingen.forEach( versnelling => {
+      if ( versnelling.waarde === 0 ) {
+        return;
       }
-      x = ((this.currentSetting.toerental / this.currentSetting.diffVerhouding / versnelling.waarde) * (this.currentSetting.bandOmtrek * 60)) / 1000
-      if (dataElement !== undefined) {
-        dataElement.y = Math.round((1000 * lastSpeed * versnelling.waarde * this.currentSetting.diffVerhouding) / (60 * this.currentSetting.bandOmtrek))
-        listItem.terugVal = dataElement.y
-        dataset.data.push(dataElement)
-        list.items.push(listItem)
-        vi++
+      x = (
+        ( this.currentSetting.toerental / this.currentSetting.diffVerhouding / versnelling.waarde )
+        * ( this.currentSetting.bandOmtrek * 60 )
+      ) / 1000;
+      if ( dataElement !== undefined ) {
+        dataElement.y = Math.round( ( 1000 * lastSpeed * versnelling.waarde * this.currentSetting.diffVerhouding )
+          / ( 60 * this.currentSetting.bandOmtrek ) );
+        listItem.terugVal = dataElement.y;
+        dataset.data.push( dataElement );
+        list.items.push( listItem );
+        vi++;
       }
 
-      lastSpeed = x
-      x = Math.round(x)
+      lastSpeed = x;
+      x = Math.round( x );
 
-      dataset.data.push({
+      dataset.data.push( {
         x: x,
         y: this.currentSetting.toerental
-      })
+      } );
 
       dataElement = {
         x: x,
         y: 0
-      }
+      };
 
       listItem = {
         versnelling: vi,
         snelheid: x,
         terugVal: 0
-      }
+      };
 
 
-    })
-    list.items.push(listItem)
-    this.plots.push(dataset)
-    this.lists.push(list)
+    } );
+    list.items.push( listItem );
+    this.plots.push( dataset );
+    this.lists.push( list );
 
-    let canvas = <HTMLCanvasElement>$('#chart').get(0);
-    let ctx = canvas.getContext('2d');
+    const canvas = <HTMLCanvasElement>$( '#chart' ).get( 0 );
+    const ctx = canvas.getContext( '2d' );
 
-    this.chart = new Chart(ctx, {
+    this.chart = new Chart( ctx, {
       type: 'scatter',
       data: {
         datasets: this.plots
@@ -144,33 +148,33 @@ export class AppComponent implements OnInit {
         responsive: true,
         hoverMode: 'single',
         scales: {
-          yAxes: [{
+          yAxes: [ {
             scaleLabel: {
               display: true,
-              labelString: "RPM"
+              labelString: 'RPM'
             }
-          }],
-          xAxes: [{
+          } ],
+          xAxes: [ {
             gridLines: {
-              zeroLineColor: "rgba(0, 0, 0, 1)"
+              zeroLineColor: 'rgba(0, 0, 0, 1)'
             },
             scaleLabel: {
               display: true,
-              labelString: "KM/U"
+              labelString: 'KM/U'
             }
-          }]
+          } ]
         },
         animation: {
           duration: 0
         }
       }
 
-    })
+    } );
   }
 
-  defaultValue() {
+  defaultValue () {
     return {
-      naam: 'Setting ' + (this.plots.length + 1),
+      naam: 'Setting ' + ( this.plots.length + 1 ),
       toerental: 7700,
       diffVerhouding: 4.428,
       bandOmtrek: 1.79,
@@ -201,6 +205,6 @@ export class AppComponent implements OnInit {
           stringWaarde: '0.864',
         }
       ]
-    }
+    };
   }
 }
