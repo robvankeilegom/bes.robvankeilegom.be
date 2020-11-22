fd8ae79442d94b3d53389d2a7aa6f8479017cfd9
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-11-22 22:24:14 +0100

    FOrmat

--- src/app/shared/plot.model.ts
+++ src/app/shared/plot.model.ts
@@ -1,15 +1,15 @@
 export interface IPlot {
-    backgroundColor: string
-    borderColor: string,
-    borderWidth: number,
-    data: IData[],
-    label: string,
-    lineTension: number,
-    showLine: boolean,
-    tension: number
+  backgroundColor: string;
+  borderColor: string;
+  borderWidth: number;
+  data: IData[];
+  label: string;
+  lineTension: number;
+  showLine: boolean;
+  tension: number;
 }
 
 export interface IData {
-    x: number,
-    y: number
+  x: number;
+  y: number;
 }
