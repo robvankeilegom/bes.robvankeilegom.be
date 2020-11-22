3fe3d8966533140e7b0e26f5e6a8eeb36e503337
Rob Van Keilegom <robvankeilegom@hotmail.com>
2020-11-22 21:14:39 +0100

    Format files

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,57 +1,63 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ISetting, IVersnelling } from '../shared/setting.model';
 
-declare let toastr
+declare let toastr;
 
-@Component({
+@Component( {
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
-})
+  styleUrls: [ './form.component.scss' ]
+} )
 export class FormComponent implements OnInit {
 
-  @Input() setting: ISetting
-  @Output() updateCurrentSetting = new EventEmitter()
-  @Output() saveAsNew = new EventEmitter()
+  @Input() setting: ISetting;
+  @Output() updateCurrentSetting = new EventEmitter();
+  @Output() saveAsNew = new EventEmitter();
 
-  constructor() { }
+  constructor () { }
 
-  ngOnInit() { }
+  public ngOnInit () { }
 
-  preventCharacters(event: any) {
-    console.log(event)
-    if (event && event.target.id !== 'naam') {
-        if ((event.which < 48 || event.which > 57) && event.which !== 0 && event.which !== 8 && event.which !== 44 && event.which !== 46 && !event.ctrlKey)
-          event.preventDefault();
-        }
-  }
-  updateGraph(event: any) {
-    if (event.which !== 48) {
-      if (this.setting.hasOwnProperty(event.target.id)) {
-        this.setting[event.target.id] = event.target.value
+  public preventCharacters ( event: any ) {
+    if ( event && event.target.id !== 'naam' ) {
+      if ( ( event.which < 48 || event.which > 57 )
+        && event.which !== 0
+        && event.which !== 8
+        && event.which !== 44
+        && event.which !== 46
+        && !event.ctrlKey ) {
+        event.preventDefault();
       }
-
-      if (event.target.name === 'versnelling') {
-        this.setting.versnellingen[event.target.id - 1].waarde = <number>event.target.value
-      }
-
-      this.updateCurrentSetting.emit(this.setting)
     }
   }
 
-  saveAsNewClick(event: any) {
-    this.saveAsNew.emit(this.setting)
+  public updateGraph ( event: any ) {
+    if ( event.which !== 48 ) {
+      if ( this.setting.hasOwnProperty( event.target.id ) ) {
+        this.setting[ event.target.id ] = event.target.value;
+      }
+
+      if ( event.target.name === 'versnelling' ) {
+        this.setting.versnellingen[ event.target.id - 1 ].waarde = <number>event.target.value;
+      }
+
+      this.updateCurrentSetting.emit( this.setting );
+    }
   }
 
-  addVersnelling() {
-    if (this.setting.versnellingen.length < 8) {
-      this.setting.versnellingen.push(<IVersnelling>{
+  public saveAsNewClick ( event: any ) {
+    this.saveAsNew.emit( this.setting );
+  }
+
+  public addVersnelling () {
+    if ( this.setting.versnellingen.length < 8 ) {
+      this.setting.versnellingen.push( <IVersnelling>{
         nummer: this.setting.versnellingen.length + 1,
-        waarde: this.setting.versnellingen[this.setting.versnellingen.length - 1].waarde
-      })
+        waarde: this.setting.versnellingen[ this.setting.versnellingen.length - 1 ].waarde
+      } );
     } else {
-      toastr.error('Dat zal wel genoeg zijn hé')
+      toastr.error( 'Dat zal wel genoeg zijn hé' );
     }
   }
 
